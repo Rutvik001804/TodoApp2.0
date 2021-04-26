fcbbd50b48662e783f3e3714c1cdbfe8a7db7806
Rutvik <rutvikmakvana001804@gmail.com>
2021-04-26 23:41:03 +0530

    Display Task

---
+++ display.js
@@ -0,0 +1,50 @@
+// Todo Task Array
+var todo = [];
+// Completed Task Array
+var completedTodo = [];
+console.log(todo);
+
+const display =  (todo,completedTodo) => {
+    var list = document.getElementById('remaining-tasks');
+    var listCompleted = document.getElementById('completed-tasks');
+
+    $("#new_title").val("");
+    $("#new_task").val("");
+    
+    list.innerHTML = '';
+    listCompleted.innerHTML = '';
+
+    if(todo.length == 0) {
+        $(list).append('<li class="no-todo-task" id="no-todo-task">No Tasks. Add Some Tasks</li>');
+    }
+    else {
+        for(let i=0; i<todo.length; i++) {
+            let html = '<div class="info-todo">' + 
+                            '<button class="completed" id="btncompleted'+i+'" onclick="completedTask(this)">' + 
+                                '<i class="fa fa-square-o"></i></button>' + 
+                            '<li class="list-todo">'+todo[i]['task']+'</li>' + 
+                            '<button class="edit" id="btnEdit'+i+'" onclick="editTask(this)">' + 
+                                '<i class="fa fa-edit"></i></button>' + 
+                            '<button class="delete" id="btnDelete'+i+'" onclick="deleteTask(this)">' + 
+                                '<i class="fa fa-trash"></i></button>'
+                        '</div>';
+            $(list).append(html);
+        }
+    }
+
+    if(completedTodo.length == 0) {
+        $(listCompleted).append('<li class="no-completed-task" id="no-completed-task">No Completed Tasks</li>');
+    }
+    else {
+        for(let j=0; j<completedTodo.length; j++) {
+            let html = '<div class="info-completed">' + 
+                            '<button class="incompleted" id="btnincompleted'+j+'" onclick="incompletedTask(this)">' + 
+                                '<i class="fa fa-square-o"></i></button>' + 
+                            '<li class="list-completed">'+completedTodo[j]['task']+'</li>' + 
+                            '<button class="delete" id="deleteBtn'+j+'" onclick="removeTask(this)">' + 
+                                '<i class="fa fa-trash"></i></button>'
+                        '</div>';
+            $(listCompleted).append(html);
+        }
+    }
+}
