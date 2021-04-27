0d75e12f89c3494fc6a582f346e7d4f4fbe76274
Rutvik <rutvikmakvana001804@gmail.com>
2021-04-27 18:50:49 +0530

    Add Feature Task Mark as Incomplete from localstorage

--- incompletedTask.js
+++ incompletedTask.js
@@ -4,8 +4,17 @@ const incompletedTask = (ele) => {
     index = index.id;
     var last = index.toString().split('btnincompleted', 2);
     
-    todo.push(completedTodo[last[1]]);
-    completedTodo.splice(last[1],1);
+    // todo.push(completedTodo[last[1]]);
+    // completedTodo.splice(last[1],1);
+
+    var todos = JSON.parse(localStorage.getItem("myTodo"));
+    var completedTodos = JSON.parse(localStorage.getItem("myCompletedTodo"));
+
+    todos.push(completedTodos[last[1]]);
+    completedTodos.splice(last[1],1);
+    
+    localStorage.setItem("myTodo", JSON.stringify(todos));
+    localStorage.setItem("myCompletedTodo", JSON.stringify(completedTodos));
     
     var a = document.getElementById('toast');    
     a.className = 'show';
@@ -14,7 +23,7 @@ const incompletedTask = (ele) => {
     }, 3000);
     a.innerHTML = "Task Back Todo Successfully";
 
-    display(this.todo, this.completedTodo);
+    display();
     
     console.log("Task Back To Go Todo");
     console.log(completedTodo);
