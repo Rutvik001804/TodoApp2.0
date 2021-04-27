c9d2d2683b8838b21cac949368abc9af5ce4d5a8
Rutvik <rutvikmakvana001804@gmail.com>
2021-04-27 18:48:42 +0530

    Add Feature Display Task from localstorage

--- display.js
+++ display.js
@@ -2,9 +2,15 @@
 var todo = [];
 // Completed Task Array
 var completedTodo = [];
-console.log(todo);
 
-const display =  (todo,completedTodo) => {
+if(localStorage.getItem("myTodo") === null) {
+    localStorage.setItem("myTodo", '[]');
+}
+if(localStorage.getItem("myCompletedTodo") === null) {
+    localStorage.setItem("myCompletedTodo", '[]');
+}
+
+const display =  () => {
     var list = document.getElementById('remaining-tasks');
     var listCompleted = document.getElementById('completed-tasks');
 
@@ -19,15 +25,18 @@ const display =  (todo,completedTodo) => {
     $("#search-todo-msg").hide();
     $("#search-completed-msg").hide();
 
-    if(todo.length == 0) {
+    var todos = JSON.parse(localStorage.getItem("myTodo"));
+    var completedTodos = JSON.parse(localStorage.getItem("myCompletedTodo"));
+
+    if(todos.length == 0) {
         $(list).append('<li class="no-todo-task" id="no-todo-task">No Tasks. Add Some Tasks</li>');
     }
     else {
-        for(let i=0; i<todo.length; i++) {
+        for(let i=0; i<todos.length; i++) {
             let html = '<div class="info-todo">' + 
                             '<button class="completed" id="btncompleted'+i+'" onclick="completedTask(this)">' + 
                                 '<i class="fa fa-square-o"></i></button>' + 
-                            '<li class="list-todo">'+todo[i]['task']+'</li>' + 
+                            '<li class="list-todo">'+todos[i]['task']+'</li>' + 
                             '<button class="edit" id="btnEdit'+i+'" onclick="editTask(this)">' + 
                                 '<i class="fa fa-edit"></i></button>' + 
                             '<button class="delete" id="btnDelete'+i+'" onclick="deleteTask(this)">' + 
@@ -37,15 +46,15 @@ const display =  (todo,completedTodo) => {
         }
     }
 
-    if(completedTodo.length == 0) {
+    if(completedTodos.length == 0) {
         $(listCompleted).append('<li class="no-completed-task" id="no-completed-task">No Completed Tasks</li>');
     }
     else {
-        for(let j=0; j<completedTodo.length; j++) {
+        for(let j=0; j<completedTodos.length; j++) {
             let html = '<div class="info-completed">' + 
                             '<button class="incompleted" id="btnincompleted'+j+'" onclick="incompletedTask(this)">' + 
-                                '<i class="fa fa-square-o"></i></button>' + 
-                            '<li class="list-completed">'+completedTodo[j]['task']+'</li>' + 
+                                '<i class="fa fa-check-square-o"></i></button>' + 
+                            '<li class="list-completed">'+completedTodos[j]['task']+'</li>' + 
                             '<button class="delete" id="deleteBtn'+j+'" onclick="removeTask(this)">' + 
                                 '<i class="fa fa-trash"></i></button>'
                         '</div>';
