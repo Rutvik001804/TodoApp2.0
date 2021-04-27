16dd055ad70f56fa56c19930560149b2571f77b6
Rutvik <rutvikmakvana001804@gmail.com>
2021-04-27 18:52:45 +0530

    Add Feature Delete Task in Incompleted Task from localstorage

--- deleteCompletedTask.js
+++ deleteCompletedTask.js
@@ -4,7 +4,11 @@ const removeTask = (dlt) => {
     index = index.id;
     var last = index.toString().split('deleteBtn', 2);
     
-    completedTodo.splice(last[1],1);
+    // completedTodo.splice(last[1],1);
+
+    var completedTodos = JSON.parse(localStorage.getItem("myCompletedTodo"));
+    completedTodos.splice(last[1],1);
+    localStorage.setItem("myCompletedTodo", JSON.stringify(completedTodos));
     
     var a = document.getElementById('toast');    
     a.className = 'show';
@@ -13,7 +17,7 @@ const removeTask = (dlt) => {
     }, 3000);
     a.innerHTML = "Task Deleted Successfully";
 
-    display(this.todo, this.completedTodo);
+    display();
 
     console.log("Task Permanently Deleted From Completed Task");
     console.log(completedTodo);
