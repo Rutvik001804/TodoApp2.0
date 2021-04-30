db0fd0e36b4fd3c4fd4c0cd826074b8872eeefb2
Rutvik <rutvikmakvana001804@gmail.com>
2021-04-30 16:25:01 +0530

    Add Feature Search and Filter

---
+++ search_and_filter.js
@@ -0,0 +1,59 @@
+// For Search and Filter
+const searchFilter = () => {
+    var list = document.getElementById('remaining-tasks');
+    var divele_todo = list.getElementsByClassName("info-todo");
+    var li_todo = list.getElementsByClassName("list-todo");
+
+    var filter = document.getElementById('filter-task').value;
+
+    var listCompleted = document.getElementById('completed-tasks');
+    var divele_completed = listCompleted.getElementsByClassName("info-completed");
+    var li_completed = listCompleted.getElementsByClassName("list-completed");
+
+    var value = document.getElementById("myInput1").value.toLowerCase().trim();
+
+    let counter_todo = li_todo.length;
+    let counter_completed  = li_completed.length;
+
+    if(li_todo.length == 0) {
+        $("#search-todo-msg").show();
+        $("#no-todo-task").hide();
+    }
+    else {
+        for(let i=0; i<li_todo.length; i++) {
+            let txt = li_todo[i].textContent || li_todo[i].innerText;
+            if (txt.toLowerCase().indexOf(value) > -1 && (filter == 'All Task' || todo[i]['Title'] == filter)) {
+                divele_todo[i].style.display = "";
+            } else {
+                divele_todo[i].style.display = "none";
+                counter_todo--;
+            }
+        }
+    }
+
+    if(li_completed.length == 0) {
+        $("#search-completed-msg").show();
+        $("#no-completed-task").hide();
+    }
+    else {
+        for(let i=0; i<li_completed.length; i++) {
+            let txt = li_completed[i].textContent || li_completed[i].innerText;
+            if (txt.toLowerCase().indexOf(value) > -1 && (filter == 'All Task' || completedTodo[i]['Title'] == filter)) {
+                divele_completed[i].style.display = "";
+            } else {
+                divele_completed[i].style.display = "none";
+                counter_completed--;
+            }
+        }
+    }
+   
+    if(counter_todo == 0) 
+        $("#search-todo-msg").show();
+    else 
+        $("#search-todo-msg").hide();
+
+    if(counter_completed == 0) 
+        $("#search-completed-msg").show();
+    else 
+        $("#search-completed-msg").hide();
+}
